494a592951dfbc1b092a05688e41e1793fd85388
unknown <>
2017-01-23 16:34:51 -0800

    Completed!

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,7 +1,7 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
 		int x = 0, y = 0;
-		while (x < 11) {
+		while (x < 10) {
 			System.out.println(y);
 			x = x + 1;
 			y = y + x;
