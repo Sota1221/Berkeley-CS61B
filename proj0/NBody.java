fe0c0ca012cf7e6a40e4d6e87c5346509d23b1a4
unknown <>
2017-01-24 20:15:55 -0800

    Completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,61 @@
 public class NBody {
+	public static double readRadius(String file) {
+		In in = new In(file);
+		in.readInt();
+		return in.readDouble();
+	}
 
+	public static Planet[] readPlanets(String file) {
+		In in = new In(file);
+		int N = in.readInt();
+		in.readDouble();
+		Planet[] pl = new Planet[N];
+		for(int i = 0; i < N; i++) {
+			double xxPos = in.readDouble();
+			double yyPos = in.readDouble();
+			double xxVel = in.readDouble();
+			double yyVel = in.readDouble();
+			double mass = in.readDouble();
+			String name = in.readString();
+			pl[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, name);
+		}
+		return pl;
+	}
+	public static void main(String[] args) {
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		Planet[] pl = readPlanets(filename);
+		double r = readRadius(filename);
+		StdDraw.setScale(-r, r);
+		StdDraw.picture(0, 0, "images/starfield.jpg");
+		for(int i = 0; i < pl.length; i++) {
+			pl[i].draw();
+		}
+		double time = 0;
+		StdAudio.play("audio/Enya - Wild Child.wav");
+		while(time < T) {
+			double[] xForces = new double[pl.length];
+			double[] yForces = new double[pl.length];
+			for(int i = 0; i < pl.length; i++) {
+				xForces[i] = pl[i].calcNetForceExertedByX(pl);
+				yForces[i] = pl[i].calcNetForceExertedByY(pl);
+			}
+			for(int i = 0; i < pl.length; i++) {
+				pl[i].update(dt, xForces[i], yForces[i]);
+			}
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			for(int i = 0; i < pl.length; i++) {
+				pl[i].draw();
+			}
+			StdDraw.show(10);
+			time += dt;
+		}
+		StdOut.printf("%d\n", pl.length);
+		StdOut.printf("%.2e\n", r);
+		for (int i = 0; i < pl.length; i++) {
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+			pl[i].xxPos, pl[i].yyPos, pl[i].xxVel, pl[i].yyVel, pl[i].mass, pl[i].imgFileName);	
+		}		
+	}
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,81 @@
 public class Planet {
+	double xxPos;
+	double yyPos;
+	double xxVel;
+	double yyVel;
+	double mass;
+	String imgFileName;
+
+	public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+		this.xxPos = xP;
+		this.yyPos = yP;
+		this.xxVel = xV;
+		this.yyVel = yV;
+		this.mass = m;
+		this.imgFileName = img;
+	}
+
+	public Planet(Planet p) {
+		this.xxPos = p.xxPos;
+		this.yyPos = p.yyPos;
+		this.xxVel = p.xxVel;
+		this.yyVel = p.yyVel;
+		this.mass = p.mass;
+		this.imgFileName = p.imgFileName;
+	}
+
+	public double calcDistance(Planet p) {
+		double dx = this.xxPos - p.xxPos;
+		double dy = this.yyPos - p.yyPos;
+		double rsq = Math.pow(dx, 2) + Math.pow(dy, 2);
+		return Math.pow(rsq, 0.5);
+	}
+
+	public double calcForceExertedBy(Planet p) {
+		return 6.67 * Math.pow(10, -11) * this.mass * p.mass / Math.pow(this.calcDistance(p), 2);
+	}
+
+	public double calcForceExertedByX(Planet p) {
+		return this.calcForceExertedBy(p) * (p.xxPos - this.xxPos) / this.calcDistance(p);
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		return this.calcForceExertedBy(p) * (p.yyPos - this.yyPos) / this.calcDistance(p);
+	}
+	
+	public double calcNetForceExertedByX(Planet[] allPlanets) {
+		double fX = 0;
+		for (int i = 0; i < allPlanets.length; i++) {
+			if(!(this.equals(allPlanets[i]))) {
+				fX += this.calcForceExertedByX(allPlanets[i]);
+			}
+		}
+		return fX;
+	}
+
+	public double calcNetForceExertedByY(Planet [] allPlanets) {
+		double fY = 0;
+		for (int i = 0; i < allPlanets.length; i++) {
+			if(!(this.equals(allPlanets[i]))) {
+				fY += this.calcForceExertedByY(allPlanets[i]);
+			}
+		}
+		return fY;
+	}
+
+	public void update(double dt, double fX, double fY) {
+		double ax = fX / this.mass;
+		double ay = fY / this.mass;
+		this.xxVel = this.xxVel + dt * ax;
+		this.yyVel = this.yyVel + dt * ay;
+		this.xxPos = this.xxPos + dt * this.xxVel;
+		this.yyPos = this.yyPos + dt * this.yyVel;
+	}
+
+	public void draw() {
+		StdDraw.picture(this.xxPos, this.yyPos, "images/" + this.imgFileName);
+
+
+	}
+
 }
