eb8e4bb818cff0c32c3e9282f9cd1980a82d5e7e
unknown <>
2017-02-08 14:06:54 -0800

    Completed

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -31,10 +31,10 @@ public class Palindrome {
         }
         Deque stringDeque = wordToDeque(word);
         int len = stringDeque.size();
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < (len - 1) / 2; i++) {
             Character char1 = (Character) stringDeque.get(i);
             Character char2 = (Character) stringDeque.get(len - 1 - i);
-            if (!cc.equalChars(char1, char2) && char1 != char2) {
+            if (!cc.equalChars(char1, char2)) {
                 return false;
             }
         }
